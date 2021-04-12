51f059a3e4f25a7e3c65dd426b15ad166eff98d8
=danaisboss <yjk9313@gmail.com>
2021-04-12 13:55:56 +0900

    ADD CRUD controller, router

--- router/Post/controller.js
+++ router/Post/controller.js
@@ -1,3 +1,4 @@
+const { Post } = require("../../models");
 const post = require("../../models/post");
 
 exports.upload = async (req, res) => {
@@ -22,6 +23,55 @@ exports.upload = async (req, res) => {
 	} catch (error) {
 		res.status(400).send({
 			errormessage: "게시글 업로드 중 오류가 발생했습니다.",
+		});
+		console.log(error);
+	}
+};
+
+exports.detail = async (req, res) => {
+	const {
+		params: { postId: id },
+	} = req;
+	try {
+		const post = await Post.findById(id);
+		res.send({ post });
+	} catch (error) {
+		res.status(400).send({
+			errormessage: "게시글을 불러오는 중 오류가 발생했습니다.",
+		});
+		console.log(error);
+	}
+};
+
+exports.editPost = async (req, res) => {
+	const {
+		params: { postId: id },
+		body: { content },
+	} = req;
+	try {
+		//TODO: 파일도 수정할 수 있도록 추가 예정
+		await Post.findByIdAndUpdate(id, { content });
+		res.send({ content });
+	} catch (error) {
+		res.status(400).send({
+			errormessage: "게시글 수정 중 오류가 발생했습니다.",
+		});
+		console.log(error);
+	}
+};
+
+exports.deletePost = async (req, res) => {
+	const {
+		params: { postId: id },
+	} = req;
+	try {
+		await Post.findByIdAndDelete(id);
+		res.send({
+			message: "삭제완료!",
+		});
+	} catch (error) {
+		res.status(400).send({
+			errormessage: "게시글 삭제 중 오류가 발생했습니다.",
 		});
 		console.log(error);
 	}
--- router/Post/index.js
+++ router/Post/index.js
@@ -1,4 +1,11 @@
 const express = require("express");
-const { upload } = require("./controller");
+const { upload, detail, editPost, deletePost } = require("./controller");
+
+const postRouter = express.Router();
 
 postRouter.post("/post/:category", upload);
+postRouter.get("/post/:postId", detail);
+postRouter.patch("/post/:postId", editPost);
+postRouter.delete("/post/:postId", deletePost);
+
+module.exports = { postRouter };
