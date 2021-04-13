349391f16d4145259f39231769d99b0896370a3b
=danaisboss <yjk9313@gmail.com>
2021-04-14 01:44:44 +0900

    Fix comment user name response value

--- router/Comment/controller.js
+++ router/Comment/controller.js
@@ -1,14 +1,21 @@
 const Comment = require("../../models/comment");
+const Post = require("../../models/post");
 
 //댓글 작성하기
 const commentUpload = async (req, res) => {
 	const userId = res.locals.user;
 	const {
+		params: { postId: id },
 		body: { content },
 	} = req;
 	try {
+		const post = Post.findById(id);
+		//Login 한 유저의 정보에서 user name 가져오는 코드
+		const userInfo = await User.findOne({ _id: userId });
+		const user = userInfo.name;
+		//DB.create 코드
 		const comment = await Comment.create({
-			user: userId,
+			user,
 			content,
 		});
 		res.send({
@@ -21,6 +28,9 @@ const commentUpload = async (req, res) => {
 		});
 	}
 };
+
+//댓글 조회하기
+const comment = async (req, res) => {};
 
 //댓글 수정하기
 const commentEdit = async (req, res) => {
