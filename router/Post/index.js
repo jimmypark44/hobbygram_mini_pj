50a9bca3ddd240cf94b53bdd338601ce2a4feeb3
jimmypark44 <71073823+jimmypark44@users.noreply.github.com>
2021-04-14 09:46:17 +0900

    add recommend, unrecommend

--- router/Post/index.js
+++ router/Post/index.js
@@ -8,5 +8,7 @@ postRouter.post("/post/:category", validations, postUpload);
 postRouter.get("/post/:postId", detail);
 postRouter.patch("/post/:postId", editPost);
 postRouter.delete("/post/:postId", deletePost);
+postRouter.post("/post/recommend/:postId", recommendPost);
+postRouter.delete("/post/recommend/:postId", unrecommendPost);
 
 module.exports = { postRouter };
