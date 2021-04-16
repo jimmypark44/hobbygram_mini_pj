c66cc3ff052ff1ad06b38782ffe9172c17c7ba20
=danaisboss <yjk9313@gmail.com>
2021-04-16 13:30:25 +0900

    Change, 글쓰기 url, 및 카테고리 request.body로 변경

--- router/Post/controller.js
+++ router/Post/controller.js
@@ -13,165 +13,164 @@ const multer = require("multer");
 // }
 
 exports.postUpload = async (req, res) => {
-    //login user정보
-    const userId = res.locals.user;
-    const {
-        params: { category },
-        body: { title, content }
-    } = req;
-    const img = req.file.path
-    // 서버에서는 '/', 윈도우에서는 '\\'
-    const temp = img.split('/')[1]
-    const fullimgpath = 'http://15.164.164.65/' + temp
-    //TODO: save image, path
-    try {
-        //Login 한 유저의 정보에서 user name 가져오는 코드
-        const userInfo = await User.findOne({ _id: userId });
-        const user = userInfo.name;
-        // DB.create 코드
-        if (!img) {
-            const newPost = await Post.create({
-                title,
-                content,
-                user,
-                category
-            })
-            res.send({ newPost })
-        }
-        const newPost = await Post.create({
-            title,
-            content,
-            user,
-            category,
-            img: fullimgpath,
-        });
-        res.send({ newPost });
-    } catch (error) {
-        if (error instanceof multer.MulterError) {
-            res.status(400).send({
-                errormessage: "파일 업로드 중 오류가 발생했습니다.",
-            });
-        } else {
-            res.status(400).send({
-                errormessage: "게시글 업로드 중 오류가 발생했습니다.",
-            });
-        }
-        console.log(error);
-    }
+	//login user정보
+	const userId = res.locals.user;
+	const {
+		body: { title, content, category },
+	} = req;
+	const img = req.file.path;
+	// 서버에서는 '/', 윈도우에서는 '\\'
+	const temp = img.split("/")[1];
+	const fullimgpath = "http://15.164.164.65/" + temp;
+	//TODO: save image, path
+	try {
+		//Login 한 유저의 정보에서 user name 가져오는 코드
+		const userInfo = await User.findOne({ _id: userId });
+		const user = userInfo.name;
+		// DB.create 코드
+		if (!img) {
+			const newPost = await Post.create({
+				title,
+				content,
+				user,
+				category,
+			});
+			res.send({ newPost });
+		}
+		const newPost = await Post.create({
+			title,
+			content,
+			user,
+			category,
+			img: fullimgpath,
+		});
+		res.send({ newPost });
+	} catch (error) {
+		if (error instanceof multer.MulterError) {
+			res.status(400).send({
+				errormessage: "파일 업로드 중 오류가 발생했습니다.",
+			});
+		} else {
+			res.status(400).send({
+				errormessage: "게시글 업로드 중 오류가 발생했습니다.",
+			});
+		}
+		console.log(error);
+	}
 };
 
 exports.showCategoryPosts = async (req, res) => {
-    const { category } = req.params
-    try {
-        const post = await Post.find({ category: category })
-        res.send({ post })
-    } catch (error) {
-        res.status(400).send({
-            errormessage: "게시글을 불러오는 중 오류가 발생했습니다.",
-        });
-        console.log(error);
-    }
-}
+	const { category } = req.params;
+	try {
+		const post = await Post.find({ category: category });
+		res.send({ post });
+	} catch (error) {
+		res.status(400).send({
+			errormessage: "게시글을 불러오는 중 오류가 발생했습니다.",
+		});
+		console.log(error);
+	}
+};
 
 exports.showAllPosts = async (req, res) => {
-    try {
-        const post = await Post.find()
-        res.send({ post })
-    } catch (error) {
-        res.status(400).send({
-            errormessage: "게시글을 불러오는 중 오류가 발생했습니다.",
-        });
-        console.log(error);
-    }
-}
+	try {
+		const post = await Post.find();
+		res.send({ post });
+	} catch (error) {
+		res.status(400).send({
+			errormessage: "게시글을 불러오는 중 오류가 발생했습니다.",
+		});
+		console.log(error);
+	}
+};
 
 //상세페이지DB 보내주기
 exports.detail = async (req, res) => {
-    const { postId } = req.params
-    try {
-        const post = await Post.findById(postId).populate([
-            { path: "comment", select: ["user", "content"] }
-        ])
-        res.send({ post });
-    } catch (error) {
-        res.status(400).send({
-            errormessage: "게시글을 불러오는 중 오류가 발생했습니다.",
-        });
-        console.log(error);
-    }
+	const { postId } = req.params;
+	try {
+		const post = await Post.findById(postId).populate([
+			{ path: "comment", select: ["user", "content"] },
+		]);
+		res.send({ post });
+	} catch (error) {
+		res.status(400).send({
+			errormessage: "게시글을 불러오는 중 오류가 발생했습니다.",
+		});
+		console.log(error);
+	}
 };
 
 //수정하기
 exports.editPost = async (req, res) => {
-    const {
-        params: { postId: id },
-        body: { title, content },
-    } = req;
-    try {
-        //TODO: 파일도 수정할 수 있도록 추가 예정
-        await Post.findByIdAndUpdate(id, { title, content });
-        res.send({ title, content, success: "true" });
-    } catch (error) {
-        res.status(400).send({
-            errormessage: "게시글 수정 중 오류가 발생했습니다.",
-        });
-        console.log(error);
-    }
+	const {
+		params: { postId: id },
+		body: { title, content },
+	} = req;
+	try {
+		//TODO: 파일도 수정할 수 있도록 추가 예정
+		await Post.findByIdAndUpdate(id, { title, content });
+		res.send({ title, content, success: "true" });
+	} catch (error) {
+		res.status(400).send({
+			errormessage: "게시글 수정 중 오류가 발생했습니다.",
+		});
+		console.log(error);
+	}
 };
 
 //삭제하기
 exports.deletePost = async (req, res) => {
-    const {
-        params: { postId: id },
-    } = req;
-    try {
-        await Post.findByIdAndDelete(id);
-        res.send({
-            success: "true",
-        });
-    } catch (error) {
-        res.status(400).send({
-            errormessage: "게시글 삭제 중 오류가 발생했습니다.",
-        });
-        console.log(error);
-    }
+	const {
+		params: { postId: id },
+	} = req;
+	try {
+		await Post.findByIdAndDelete(id);
+		res.send({
+			success: "true",
+		});
+	} catch (error) {
+		res.status(400).send({
+			errormessage: "게시글 삭제 중 오류가 발생했습니다.",
+		});
+		console.log(error);
+	}
 };
 
 // 추천하기
 exports.recommendPost = async (req, res) => {
-    const {
-        params: { postId: id },
-    } = req;
-    // const { postId } = req.params
-    // const userId = res.locals.user
-    const userId = res.locals.user;
-    console.log(userId);
-    try {
-        const post = await Post.findOne({ _id: id });
-        if (post.recommendUser.includes(userId)) {
-            await Post.updateOne(
-                { _id: id },
-                {
-                    $pull: { recommendUser: userId },
-                    $inc: { recommendCnt: -1 },
-                }
-            );
-            return res.send({ success: false })
-        }
-        await Post.updateOne(
-            { _id: id },
-            {
-                $push: { recommendUser: userId },
-                $inc: { recommendCnt: 1 },
-            }
-        );
-        return res.send({ success: true });
-    } catch (error) {
-        res.status(400).send({
-            errormessage: "게시글 추천 중 오류가 발생했습니다.",
-        });
-        console.log(error);
-    }
+	const {
+		params: { postId: id },
+	} = req;
+	// const { postId } = req.params
+	// const userId = res.locals.user
+	const userId = res.locals.user;
+	console.log(userId);
+	try {
+		const post = await Post.findOne({ _id: id });
+		if (post.recommendUser.includes(userId)) {
+			await Post.updateOne(
+				{ _id: id },
+				{
+					$pull: { recommendUser: userId },
+					$inc: { recommendCnt: -1 },
+				}
+			);
+			return res.send({ success: false });
+		}
+		await Post.updateOne(
+			{ _id: id },
+			{
+				$push: { recommendUser: userId },
+				$inc: { recommendCnt: 1 },
+			}
+		);
+		return res.send({ success: true });
+	} catch (error) {
+		res.status(400).send({
+			errormessage: "게시글 추천 중 오류가 발생했습니다.",
+		});
+		console.log(error);
+	}
 };
 
 // exports.unrecommendPost = async (req, res) => {
--- router/Post/index.js
+++ router/Post/index.js
@@ -1,18 +1,26 @@
-const { Router } = require("express")
+const { Router } = require("express");
 const PostController = require("./controller");
-const PostRouter = Router()
+const PostRouter = Router();
 const validations = require("../../middlewares/validations");
-const upload = require("../../middlewares/imgUpload")
-
+const upload = require("../../middlewares/imgUpload");
 
 // PostRouter.post("/post/uploadImg", validations, upload.single("img"), PostController.uploadImg)
-PostRouter.post("/post/:category", validations, upload.single("img"), PostController.postUpload);
+PostRouter.post(
+	"/post/write",
+	validations,
+	upload.single("img"),
+	PostController.postUpload
+);
 PostRouter.get("/post/:category", PostController.showCategoryPosts);
 PostRouter.get("/post", PostController.showAllPosts);
 PostRouter.get("/post/detail/:postId", PostController.detail);
 PostRouter.patch("/post/:postId", PostController.editPost);
 PostRouter.delete("/post/:postId", PostController.deletePost);
-PostRouter.post("/post/recommend/:postId", validations, PostController.recommendPost);
+PostRouter.post(
+	"/post/recommend/:postId",
+	validations,
+	PostController.recommendPost
+);
 // PostRouter.delete("/post/recommend/:postId", validations, PostController.unrecommendPost);
 
-module.exports = PostRouter
+module.exports = PostRouter;
