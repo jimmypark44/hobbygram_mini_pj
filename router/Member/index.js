d0e05f7b4f19c60844a63817330a093c4176001e
jimmypark44 <71073823+jimmypark44@users.noreply.github.com>
2021-04-17 15:03:09 +0900

    Add getUser & fix typo

--- router/Member/index.js
+++ router/Member/index.js
@@ -1,14 +1,15 @@
 const { Router } = require("express")
-const MemeberController = require("./controller")
+const MemberController = require("./controller")
 const MemberRouter = Router()
-const isVaildation = require("../../middlewares/validations")
+const isValidation = require("../../middlewares/validations")
 // 자세한 코드는 controller에서 확인 가능
 
 // 로그인 / 토큰 발행
-MemberRouter.post("/login", MemeberController.login)
+MemberRouter.post("/login", MemberController.login)
 // 회원가입
-MemberRouter.post("/join", MemeberController.join)
+MemberRouter.post("/join", MemberController.join)
 // 이미 있는 이메일인지 확인
-MemberRouter.post("/checkEmail", MemeberController.checkEmail)
+MemberRouter.post("/checkEmail", MemberController.checkEmail)
+MemberRouter.post("/getUser", isValidation, MemberController.getUser)
 
 module.exports = MemberRouter
