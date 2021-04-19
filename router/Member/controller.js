01dd875478c3560334cfbc146fe1e271d049fdcd
=danaisboss <yjk9313@gmail.com>
2021-04-19 11:09:48 +0900

    Update, User password 해싱하여 저장, bcrypt 사용

--- router/Member/controller.js
+++ router/Member/controller.js
@@ -1,6 +1,8 @@
 const { User } = require("../../models");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 // jwt 사용한 로그인과 회원가입
 exports.login = async (req, res, next) => {
@@ -15,10 +17,7 @@ exports.login = async (req, res, next) => {
 			return res
 				.status(400)
 				.send({ err: "이메일 혹은 비밀번호가 일치하지 않습니다." });
-		const token = jwt.sign(
-			{ userId: user._id },
-			process.env.TOKEN_KEY
-		);
+		const token = jwt.sign({ userId: user._id }, process.env.TOKEN_KEY);
 		return res.send({ result: { user: { token: token, name: user.name } } });
 	} catch (err) {
 		console.log(err);
@@ -43,7 +42,14 @@ exports.join = async (req, res, next) => {
 
 	const NewUser = new User({ ...req.body });
 	try {
-		await NewUser.save();
+		//bycrpt
+		bcrypt.genSalt(saltRounds, function (err, salt) {
+			bcrypt.hash(NewUser.password, salt, function (err, hash) {
+				// Store hash in your password DB.
+				NewUser.password = hash;
+				NewUser.save();
+			});
+		});
 		return res.send({ success: true });
 	} catch (err) {
 		return res.status(400).send({ err: err.message });
@@ -63,10 +69,10 @@ exports.checkEmail = async (req, res, next) => {
 exports.getUser = async (req, res, next) => {
 	const userId = res.locals.user;
 	// const token = res.locals.token
-	const user = await User.findOne({ _id: userId })
+	const user = await User.findOne({ _id: userId });
 	if (user) {
-		return res.send({ name: user.name, email: user.email })
+		return res.send({ name: user.name, email: user.email });
 	} else {
 		return res.status(400).send({ err: "해당 아이디가 없습니다" });
 	}
-}
+};
